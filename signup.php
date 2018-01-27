a2a1a07e2f87b3d5afda4272b91361e334cdce15
Stjepan Mudronja <smudronja.sm@gmail.com>
2018-01-27 22:03:27 +0100

    Created sing up PHP script

--- signup.php
+++ signup.php
@@ -7,7 +7,7 @@
 	<section class="main-container">
 		<div class="main-wrapper">
 			<h2>Signup</h2>
-			<form class="signup-form">
+			<form class="signup-form" action="includes/signup.inc.php" method="POST">
 				<input type="text" name="first" placeholder="Firstname">
 				<input type="text" name="last" placeholder="Lastname">
 				<input type="text" name="email" placeholder="E-mail">
