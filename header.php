35205983e9451b224b81f0b2aeb66e0e910b647b
Stjepan Mudronja <smudronja.sm@gmail.com>
2018-01-27 17:42:46 +0100

    front end code finished

---
+++ header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>login system</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	
+	<header id="header" class="">
+		<nav>
+			<div class="main-wrapper">
+				<ul>
+					<li><a href="index.php" title="">Home</a></li>
+				</ul>
+				<div class="nav-login">
+					<form>
+						<input type="text" name="uid" placeholder="Username/e-mail">
+						<input type="password" name="pwd" placeholder="password">
+						<button type="submit" name="submit">Login</button>
+					</form>
+					<a href="signup.php">Sing up</a>
+				</div>
+			</div>
+		</nav>
+	</header><!-- /header -->
---
+++ signup.php
@@ -0,0 +1,23 @@
+<?php 
+	include_once 'header.php';
+ ?>
+
+
+
+	<section class="main-container">
+		<div class="main-wrapper">
+			<h2>Signup</h2>
+			<form class="signup-form">
+				<input type="text" name="first" placeholder="Firstname">
+				<input type="text" name="last" placeholder="Lastname">
+				<input type="text" name="email" placeholder="E-mail">
+				<input type="text" name="uid" placeholder="Username">
+				<input type="password" name="pwd" placeholder="Password">
+				<button type="submit" name="submit">Sign up</button>
+			</form>
+		</div>
+	</section>
+
+<?php 
+	include_once 'footer.php';
+?>
